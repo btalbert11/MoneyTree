caf6ce58d5ab538c6ed95a749a711c62994623bc
btalbert11 <btalb2653@gmail.com>
2024-01-07 15:33:18 -0500

    Can buy trees, each has individual amount of money and regrow timer

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 // Single money tree. Hold a certain amount of money, when it runs out there is a timer until it is refreshed
 class MoneyTree {
-    constructor(timeToGrow = 10000, germinateTime = 1000, moneyAmount = 1000, maxMoney = 1000) {
+    constructor(timeToGrow = 10000, germinateTime = 1000, moneyAmount = 0, maxMoney = 100) {
         this.maxMoney = maxMoney
         this.timeToGrow = timeToGrow // When buying a new tree
         this.germinateTime = germinateTime // time to refresh all money in tree
         this.moneyAmount = moneyAmount // money in tree
-        this.refreshTimer // used to track when tree has money again, will be an intervalTimeout
+        this.refreshTimer = undefined // used to track when tree has money again, will be an intervalTimeout
+        this.growTimer  = undefined // used to track when tree is done growing with interval timeout
         this.active = false // set when grown
-        this.outOfMoney = false // set to true when regrowing money
+        this.hasMoney = true // set to true when after money grown
+
+        if (this.moneyAmount == 0 || this.moneyAmount > this.maxMoney) {
+            this.moneyAmount = this.maxMoney
+        }
     }
 
     // Called by a timeout function to restore money
-    regrowTimer() {
-        this.outOfMoney = false
+    regrowMoney() {
+        this.hasMoney = true
         this.moneyAmount = this.maxMoney
+        this.refreshTimer = undefined
         console.log("tree is back", new Date(Date.now()).toString(), this.moneyAmount)
+        updateTreeListIcons()
+    }
+
+    activate() {
+        this.active = true
+        this.growTimer = undefined
+        updateTreeListIcons()
     }
 }
 
+// Global vars to reference
 var mainGameLoop // performs all continous actions ever tick. set in the init function
+var treeList = document.getElementById("treeList")
 
 // Global game data
 var gameData = {
@@ -28,6 +43,7 @@ var gameData = {
     moneyInterval: 1000, // how often money is automatically made, in ms
     moneyIntervalCost: 10, // cost to reduce interval
     Trees: [new MoneyTree()], // list of money trees
+    treeCost: 10, // cost of new tree
     lastTickTime: Date.now(), // real time of last tick
     globalTickRate: 1, // tick rate of game, in ms. This may become a problem being so often
     tickCount: 0, // used to count ticks for money collection
@@ -37,29 +53,56 @@ var pageStateData = {
     currentPage: "moneyTreeMenu",
 }
 
-
+// Update icons of trees for having money/not
+function updateTreeListIcons() {
+    // Remove all elements of tree list to re-add
+    while (treeList.firstChild) {
+        treeList.removeChild(treeList.firstChild)
+    }
+    gameData.Trees.forEach(function (tree) {
+        // Tree has money
+        var li = document.createElement("li")
+        li.setAttribute("id", "tree")
+        li.setAttribute("class", "tree")
+        if (tree.hasMoney) {
+            li.append("🌳")
+        }
+        // Tree has no money
+        else {
+            li.append("DEAD")            
+        }
+        treeList.appendChild(li)
+    })
+}
 
 // Loop through all trees, add money to total, and reduce money in tree
 function harvestMoney() {
     gameData.Trees.forEach(function (tree, i) {
-        // console.log(i, tree)
-        if (!tree.active) {return}
-        if (tree.moneyAmount > gameData.moneyPerClick) { // can get full amount, tree will not run out
+        console.log(i, tree)
+        // Tree still growing after purchase or after running out money
+        if (!tree.active || !tree.hasMoney) {return}
+        // Tree has more than full amount
+        if (tree.moneyAmount > gameData.moneyPerClick) {
             gameData.money += gameData.moneyPerClick
             tree.moneyAmount -= gameData.moneyPerClick
         }
+        // Tree will run out, update icon
         else if (tree.moneyAmount > 0 && tree.moneyAmount <= gameData.moneyPerClick) { // tree will run out
             gameData.money += tree.moneyAmount
             tree.moneyAmount = 0
             tree.refreshTimer = 0
-            tree.outOfMoney = true
+            tree.hasMoney = false
             // set tree timeout
             tree.refreshTimer = window.setTimeout(function() {
-                tree.regrowTimer()
+                tree.regrowMoney()
             }, tree.germinateTime)
-            document.getElementById("tree").innerHTML = "DEAD"
+            // document.getElementById("tree").innerHTML = "DEAD"
+            updateTreeListIcons()
         }
-        else {}
+        // Error?
+        else {
+            console.error("HARVEST MONEY ERROR")
+        }
     })
 
     //gameData.money += gameData.moneyPerClick
@@ -77,6 +120,21 @@ function buyWorker() {
     }
 }
 
+function plantTree() {
+    if (gameData.money >= gameData.treeCost) {
+        var newTree = new MoneyTree()
+        // Change money
+        gameData.Trees.push(newTree)
+        gameData.money -= gameData.treeCost
+        gameData.treeCost *= 10
+        // Setup growth timeout
+        newTree.growTimer = window.setTimeout(function () {
+            newTree.activate()
+        }, newTree.timeToGrow)
+        // Update html
+        document.getElementById("plantTree").innerHTML = "Plant a new Money Tree: " + (gameData.treeCost) + " moneys"
+    }
+}
 
 function buyFasterMoney() {
     if (gameData.money > gameData.moneyIntervalCost) {
@@ -111,7 +169,6 @@ function init() {
     tab("moneyTreeMenu")
     gameData.Trees.forEach (function (tree, i) {
         tree.active = true
-        tree.timeToGrow = 0
     })
 }
 
